a80d859e2bd18d2c306467039e077384c38c5418
Wolf Mermelstein <wolf@404wolf.com>
2025-10-05 19:40:53 -0400

    Add side buttons

---
+++ src/content/links.ts
@@ -0,0 +1,7 @@
+import contacts from "./contacts.json" with { type: "json" }
+
+export default [
+    { "text": "Source", "href": "https://github.com/404Wolf/404wolf.com-v2" },
+    { "text": "Email", "href": contacts.find(c => c.name === "Email")?.link || "#" },
+    { "text": "Phone", "href": contacts.find(c => c.name === "Phone")?.link || "#" },
+]
