667ca512a9dd551b111ee9c6e54146fd5cbaac73
Wolf Mermelstein <wolf@404wolf.com>
2025-10-05 22:39:49 -0400

    Add toc

--- astro.config.ts
+++ astro.config.ts
@@ -7,35 +7,37 @@ import { defineConfig } from "astro/config";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import { SITE_URL } from "./src/consts.ts";
+import remarkToc from "remark-toc";
 
 export default defineConfig({
-	site: SITE_URL,
-	integrations: [
-		mdx({
-			remarkPlugins: [
-				remarkFrontmatter,
-				[remarkMdxFrontmatter, { name: "frontmatterData" }],
-			],
-			gfm: true,
-			shikiConfig: {
-				theme: "github-light-default",
-			}
-		}),
-		sitemap(),
-		react(),
-	],
-	output: "static",
-	publicDir: "public",
-	vite: {
-		plugins: [tailwindcss()],
-	},
-	redirects: {
-		"/bio": "/about",
-		"/resume": "/resume.pdf",
-	},
-	adapter: cloudflare({
-		platformProxy: {
-			enabled: true,
-		},
-	}),
+  site: SITE_URL,
+  integrations: [
+    mdx({
+      remarkPlugins: [
+        remarkFrontmatter,
+        [remarkMdxFrontmatter, { name: "frontmatterData" }],
+        [remarkToc, { heading: "Contents" }]
+      ],
+      gfm: true,
+      shikiConfig: {
+        theme: "github-light-default",
+      }
+    }),
+    sitemap(),
+    react(),
+  ],
+  output: "static",
+  publicDir: "public",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  redirects: {
+    "/bio": "/about",
+    "/resume": "/resume.pdf",
+  },
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });
