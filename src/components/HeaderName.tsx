053ec264b8498d605f43f9588c61efa98fd88f47
Wolf Mermelstein <wolf@404wolf.com>
2025-10-26 22:25:34 -0400

    Fix title for blog post

--- src/components/HeaderName.tsx
+++ src/components/HeaderName.tsx
@@ -1,40 +1,46 @@
 import Typewriter from "typewriter-effect";
 
 export function HeaderName({
-	className,
-	linkTo = "/",
+  className,
+  linkTo = "/",
+  customText,
 }: {
-	className?: string;
-	linkTo?: string;
+  className?: string;
+  linkTo?: string;
+  customText?: string;
 }) {
-	const Header = (
-		<div className={className}>
-			<div className="relative">
-				<div className="text-transparent mx-px whitespace-nowrap">
-					Hi! I'm Wolf Mermelstein
-				</div>
+  const Header = (
+    <div className={className}>
+      <div className="relative">
+        <div className="text-transparent mx-px whitespace-nowrap">
+          {customText === undefined ? "Hi! I'm Wolf Mermelstein" : customText}
+        </div>
 
-				<div className="absolute left-0 top-0">
-					<Typewriter
-						onInit={(typewriter) => {
-							typewriter
-								.typeString("Hi! ")
-								.pauseFor(700)
-								.typeString("I'm Wolf Mermelstein")
-								.start();
-						}}
-						options={{ delay: 70, cursor: "", autoStart: true }}
-					/>
-				</div>
-			</div>
-		</div>
-	);
+        <div className="absolute left-0 top-0">
+          <Typewriter
+            onInit={(typewriter) => {
+              if (customText === undefined) {
+                typewriter
+                  .typeString("Hi! ")
+                  .pauseFor(700)
+                  .typeString("I'm Wolf Mermelstein")
+                  .start();
+              } else {
+                typewriter.typeString(customText).start();
+              }
+            }}
+            options={{ delay: 70, cursor: "", autoStart: true }}
+          />
+        </div>
+      </div>
+    </div>
+  );
 
-	return linkTo === undefined ? (
-		Header
-	) : (
-		<a href={linkTo} aria-label="Home">
-			{Header}
-		</a>
-	);
+  return linkTo === undefined ? (
+    Header
+  ) : (
+    <a href={linkTo} aria-label="Home">
+      {Header}
+    </a>
+  );
 }
