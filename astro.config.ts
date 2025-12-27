ee5bb197a357e5ce9e0f7ddd92832dc4c4d272c4
Wolf Mermelstein <wolf@404wolf.com>
2025-12-26 22:13:19 -0500

    format code

--- astro.config.ts
+++ astro.config.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
@@ -8,57 +9,56 @@ import remarkFrontmatter from "remark-frontmatter";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 import { SITE_URL } from "./src/consts.ts";
 import { fetchLatestResume } from "./src/resume.ts";
-import path from "node:path";
 
 export default defineConfig({
-  site: SITE_URL,
-  integrations: [
-    mdx({
-      remarkPlugins: [
-        remarkFrontmatter,
-        [remarkMdxFrontmatter, { name: "frontmatterData" }],
-      ],
-      gfm: true,
-      shikiConfig: {
-        theme: "github-light-default",
-      },
-    }),
-    sitemap(),
-    react(),
-  ],
-  output: "static",
-  publicDir: "public",
-  vite: {
-    plugins: [
-      tailwindcss(),
-      fetchLatestResume({
-        owner: "404wolf",
-        repo: "resume-v2",
-        releasePath: "resume.pdf",
-        outputPath: "resume.pdf",
-      }),
-    ],
-    resolve: {
-      alias: {
-        'unicorn-magic': path.resolve('./src/hack/unicorn-magic.js'),
-      },
-    },
-    optimizeDeps: {
-      exclude: ["@deno/kv", "@deno/kv-linux-x64-gnu"],
-    },
-    ssr: {
-      noExternal: [],
-      external: ["@deno/kv", "@deno/kv-linux-x64-gnu"],
-    },
-  },
-  redirects: {
-    "/bio": "/about",
-    "/resume": "/resume.pdf",
-    "/posts": "/",
-  },
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
+	site: SITE_URL,
+	integrations: [
+		mdx({
+			remarkPlugins: [
+				remarkFrontmatter,
+				[remarkMdxFrontmatter, { name: "frontmatterData" }],
+			],
+			gfm: true,
+			shikiConfig: {
+				theme: "github-light-default",
+			},
+		}),
+		sitemap(),
+		react(),
+	],
+	output: "static",
+	publicDir: "public",
+	vite: {
+		plugins: [
+			tailwindcss(),
+			fetchLatestResume({
+				owner: "404wolf",
+				repo: "resume-v2",
+				releasePath: "resume.pdf",
+				outputPath: "resume.pdf",
+			}),
+		],
+		resolve: {
+			alias: {
+				"unicorn-magic": path.resolve("./src/hack/unicorn-magic.js"),
+			},
+		},
+		optimizeDeps: {
+			exclude: ["@deno/kv", "@deno/kv-linux-x64-gnu"],
+		},
+		ssr: {
+			noExternal: [],
+			external: ["@deno/kv", "@deno/kv-linux-x64-gnu"],
+		},
+	},
+	redirects: {
+		"/bio": "/about",
+		"/resume": "/resume.pdf",
+		"/posts": "/",
+	},
+	adapter: cloudflare({
+		platformProxy: {
+			enabled: true,
+		},
+	}),
 });
--- src/hack/unicorn-magic.js
+++ src/hack/unicorn-magic.js
@@ -1,19 +1,19 @@
 // https://github.com/sindresorhus/unicorn-magic/blob/6614e1e82a19f41d7cc8f04df7c90a4dfe781741/node.js
 
 export function toPath(path) {
-  return path;
+	return path;
 }
 
 export function traversePathUp(startPath) {
 	return {
-		* [Symbol.iterator]() {
+		*[Symbol.iterator]() {
 			let currentPath = path.resolve(toPath(startPath));
 			let previousPath;
 
 			while (previousPath !== currentPath) {
 				yield currentPath;
 				previousPath = currentPath;
-				currentPath = path.resolve(currentPath, '..');
+				currentPath = path.resolve(currentPath, "..");
 			}
 		},
 	};
